81ff13d32322c519cd1568583d520cb4283dc7d9
Jaimewww <jaimelandazuri719@gmail.com>
2024-10-18 07:25:55 -0500

    bidimensionales

---
+++ bidimensionales.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#ifndef BIDIMENSIONALES_H
+#define BIDIMENSIONALES_H
+#define PI 3.1415
+
+//AREA DEL CIRCULO
+double circulo_area(){
+    
+
+}
+
+
+
+
+
+#endif
