b622d0b146a2540b53688f49c0a7d84fb6aef9ce
TheGreatKamus <emiliofloresga@gmail.com>
2025-02-01 22:03:27 -0500

    metodos arreglados

--- src/controller/F_Emilio.java
+++ src/controller/F_Emilio.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controller;
+import utiles.Utiles;
 
 /**
  *
@@ -12,49 +13,58 @@ public class F_Emilio {
 
     // dodecaedro area 17
     public static float Ejecutar_dodecaedro(float a) {
-        return (float) (3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(a, 2));
+        float area = (float) (3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(a, 2) );
+        return Utiles.redondear(area); 
     }
 
     // dodecaedro volumne 18
     public static float Ejecutar_dodecaedro_volumen(float a) {
-        return (float) (((15 + 7 * Math.sqrt(5)) * Math.pow(a, 3)) / 4);
+        float area = (float) (((15 + 7 * Math.sqrt(5)) * Math.pow(a, 3)) / 4);
+        return Utiles.redondear(area);
     }
 
     // dodecagono 19
     public static float Ejecutar_dodecagono(float l) {
-        return (float) (5 * l * (l / (2 * Math.tan(Math.toRadians(18)))));
+        float area =  (float) (5 * l * (l / (2 * Math.tan(Math.toRadians(18)))));
+        return Utiles.redondear(area); 
     }
 
     // elipse 20
     public static float Ejecutar_elipse(float a, float b) {
-        return (float) (Math.PI * a * b);
+        float area = (float) (Math.PI * a * b);
+        return Utiles.redondear(area); 
     }
 
     // elipsoide 21
     public static float Ejecutar_elipsoide_volumen(float l, float h, float a) {
-        return (float) (4 * Math.PI / 3) * l * a * h;
+        float area = (float) (4 * Math.PI / 3) * l * a * h;
+        return Utiles.redondear(area); 
     }
 
     // eneagono 22
     public static float Ejecutar_eneagono(float l) {
         double ap = (l / 2) * (Math.sin(Math.toRadians(70)) / Math.sin(Math.toRadians(20)));
-        return (float) (9 * ap * l) / 2;
+        float vol = (float) (9 * ap * l) / 2;
+        return Utiles.redondear(vol); 
     }
 
     // esfera hueca 23
     public static float Ejecutar_esfera_hueca_volumen(float r, float R) {
         double volumen = ((double) 4 / 3) * Math.PI * ((Math.pow(R, 3)) - (Math.pow(r, 3)));
-        return (float) volumen;
+        float vol = (float) volumen;
+        return Utiles.redondear(vol); 
     }
 
     // esfera inclinada 24
     public static float Ejecutar_esfera_inclinada_volumen(float r, float h) {
-        return (float) (Math.PI * Math.pow(h, 2) * (3 * r - h)) / 3;
+        float vol = (float) (Math.PI * Math.pow(h, 2) * (3 * r - h)) / 3;
+        return Utiles.redondear(vol); 
     }
 
     //esferoide oblato 25 
     public static float Ejecutar_esferoide_oblato_volumen(float e, float p) {
         double volumen = ((double) 4 / 3) * Math.PI * Math.pow(e, 2) * p;
-        return (float) volumen;
+        float vol =  (float) volumen;
+        return Utiles.redondear(vol); 
     }
 }
